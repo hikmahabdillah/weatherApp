fe569bdffc3cf3902b895602bf62b9ee622c831a
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-23 12:01:22 +0700

    try to deploy again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist', // adjust this to your desired output directory
+    assetsDir: 'img',
+    publicPath: '/', // serve static assets from the root directory
     index: 'index.html', // adjust this to your desired index file
   },
 });
