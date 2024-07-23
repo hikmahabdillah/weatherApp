dcedb28b20e5c27d65be80e23a1c8865d9088f25
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-07-23 11:50:58 +0700

    try to deploy again

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Konfigurasi tambahan jika diperlukan
+  build: {
+    outDir: 'dist', // adjust this to your desired output directory
+    index: 'index.html', // adjust this to your desired index file
+  },
 });
